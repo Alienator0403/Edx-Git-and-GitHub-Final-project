aa887da14d1a0438185d306f0a50e911b22478f5
Jay <117523276+Alienator0403@users.noreply.github.com>
2024-10-14 17:44:31 +0530

    Added Erro handling to the calcuator code
    Now values which are not numerical will not be taken into consideration and a message will be printed to terminal

--- simple-interest.sh
+++ simple-interest.sh
@@ -14,11 +14,16 @@ read -p "Enter the principle amount: " principle
 read -p "Enter the Rate of interest per annum: " rate
 read -p "Enter the tenure or time of interest: (in years) " time
 
-# Calculating Interest I = (P*T*R)/100
-interest_amount=$((principle * rate * time / 100))
+# Error Handling : Outputs only if all the values entered are Numbers
+if [[ $principle =~ ^[0-9]+$  && $rate =~ ^[0-9]+$  && $time =~ ^[0-9]+$ ]]; then
+    # Calculating Interest I = (P*T*R)/100
+    interest_amount=$((principle * rate * time / 100))
 
-# Final amout to be paid A = P + I
-total_amount=$((principle + interest_amount))
+    # Final amout to be paid A = P + I
+    total_amount=$((principle + interest_amount))
 
-# Showing results to the user
-echo -e "total amount is : $total_amount\nThe interest is : $interest_amount"
+    # Showing results to the user
+    echo -e "\ntotal amount is : $total_amount\nThe interest is : $interest_amount"
+else
+    echo -e "\none or more values entered is/are not a number\nTry again!!"
+fi

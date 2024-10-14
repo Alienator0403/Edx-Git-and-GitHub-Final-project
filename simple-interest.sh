e0e17e68d1d024b4b31b1e5ac780329b815806c8
Jay <117523276+Alienator0403@users.noreply.github.com>
2024-10-14 17:56:02 +0530

    Updated the code to have floating point capabilities for all the input values

--- simple-interest.sh
+++ simple-interest.sh
@@ -15,9 +15,9 @@ read -p "Enter the Rate of interest per annum: " rate
 read -p "Enter the tenure or time of interest: (in years) " time
 
 # Error Handling : Outputs only if all the values entered are Numbers
-if [[ $principle =~ ^[0-9]+$  && $rate =~ ^[0-9]+$  && $time =~ ^[0-9]+$ ]]; then
+if [[ $principle =~ ^[0-9]+(\.[0-9]+)?$  && $rate =~ ^[0-9]+(\.[0-9]+)?$  && $time =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
     # Calculating Interest I = (P*T*R)/100
-    interest_amount=$((principle * rate * time / 100))
+    interest_amount=$( echo "$principle * $rate * $time / 100" | bc )
 
     # Final amout to be paid A = P + I
     total_amount=$((principle + interest_amount))
